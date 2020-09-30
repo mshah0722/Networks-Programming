dfae1b9651d0ac790be3352c247d7415099806ff
Krutarth Patel <krutarth2000@gmail.com>
2020-09-30 12:16:59 -0400

    minor changes

--- server.c
+++ server.c
@@ -5,76 +5,70 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
-#define MAXLEN 4096
+#define MAXBUFLEN 100
 
 int main(int argc, char const *argv[]){
     
     int port;
-    
-    // switch(argc) {
-        // case 2:
-            // port = atoi(argv[1]);
-            // break;
-        // default:
-            // fprintf(stderr, "usage: server (udp listen port)\n");
-            // return(0);
-    // }
-    
+	struct sockaddr_in server_addr;
+    char buf[MAXBUFLEN] = {0};
+	int sockfd;//socket file descriptor
+    struct sockaddr_in clientAddress;
+	
+	char *msg_yes[]="yes";
+	char *msg_no[]="no";
+	
 	if(argc == 2) //check for correct arguments
 		port = atoi(argv[1]);
 	else{//print error message and return
-		printf(stderr, "incorrect arguments amoung. usage: server <udp listen port>\n");
+		fprintf(stderr, "incorrect argument amount. usage: server <udp listen port>\n");
 		return 1;
 	}
-	
-    int socketDescriptor;
+	 
     
     //Creates a ipv4 dgram udp socket
-    if ((socketDescriptor = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
         fprintf(stderr, "socket error\n");
         return 1;
-    }
-    
-    struct sockaddr_in serverAddress; 
+    } 
     
     
-    serverAddress.sin_family = AF_INET;//sets to IPV4
+    server_addr.sin_family = AF_INET;//sets to IPV4
     
-    serverAddress.sin_port = htons(port);//set port to network byte order
+    server_addr.sin_port = htons(port);//set port to network byte order
     
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);// references  IP address (in Network Byte Order)
-    //Sets it to the right memory space
-    memset(serverAddress.sin_zero, 0, sizeof(serverAddress.sin_zero));
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);// references  IP address (in Network Byte Order)
     
-    //Assigns address to the socket
-    if (bind(socketDescriptor, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1 ) {
-        fprintf(stderr, "Can't bind name to socket\n");
+    memset(server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));//makes sure the struct is empty
+    
+    
+    if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1 ) {//Assigns address to the socket
+        fprintf(stderr, "bind error\n");
         return 1;
     }
     
     
-    char message_buffer[MAXLEN] = {0};
-    struct sockaddr_in clientAddress;
-    unsigned clientMessageLength = sizeof(struct sockaddr_in);
+    unsigned msg_len = sizeof(struct sockaddr_in);//will contain the length of the address received
     
-    if(recvfrom(socketDescriptor, message_buffer, MAXLEN, 0, (struct sockaddr*) &clientAddress, &clientMessageLength) < 0) {
-        fprintf(stderr, "Can't receive data\n");
+    if(recvfrom(sockfd, buf, MAXBUFLEN, 0, (struct sockaddr*) &clientAddress, &msg_len) == -1) {//receive message from client
+        fprintf(stderr, "recv error\n");
         return 1;
     }
     
-    if (strcmp(message_buffer, "ftp") == 0) {
-        if (sendto(socketDescriptor, "yes", strlen("yes"), 0, (struct sockaddr*) &clientAddress, clientMessageLength) == -1) {
-                fprintf(stderr, "Message not sent to client\n");
-                return(1);
+    if (strcmp(buf, "ftp") == 0) {//compare the messages received
+        if (sendto(sockfd, msg_yes, strlen("msg_yes"), 0, (struct sockaddr*) &clientAddress, msg_len) == -1) {//check if message sent
+                fprintf(stderr, "Message not sent\n");
+                return 1;
         }
-        else if (sendto(socketDescriptor, "no", strlen("no"), 0, (struct sockaddr*) &clientAddress, clientMessageLength) == -1) {
-            fprintf(stderr, "Message not sent to client\n");
+        else if (sendto(sockfd, msg_no, strlen(msg_no), 0, (struct sockaddr*) &clientAddress, msg_len) == -1) {//check if message sent
+            fprintf(stderr, "Message not sent\n");
             return 1;
         }
     }
 
-    close(socketDescriptor);
+    close(sockfd);//close connection
     return 0;
 }
