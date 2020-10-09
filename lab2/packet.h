cf3c4de1468432089c8c0decaa894fd3a077f42a
Krutarth Patel <krutarth2000@gmail.com>
2020-10-09 19:17:39 -0400

    edited files

--- lab2/packet.h
+++ lab2/packet.h
@@ -9,37 +9,180 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <ctype.h>
 #include <netdb.h>
 #include <errno.h>
 #include <time.h>
 
 
+#define FRAGMENTSIZE 1000
+
 struct packet {
-    unsigned int total_frag;        // Total number of fragments
-    unsigned int frag_no;        // Fragment number of packet
-    unsigned int size;          // Size of data in bytes
-    char* filename;                     //File that is being transferred
-    char fileData[1000];        //Data of the provided file
-    struct packet * nextPacket;         //Pointer to the next packet
+    unsigned int total_frag;        
+    unsigned int frag_no;        
+    unsigned int size;          
+    char* filename;                  
+    char filedata[FRAGMENTSIZE];       
+    struct packet * next;         
 };
 
-//Prints all of the packet details (members of the packet struct)
-void printPacket(struct packet * p);
+// //Prints all of the packet details (members of the packet struct)
+// void printPacket(struct packet * p){
+    // printf("Total Fragments: %d\n", p->total_frag);
+    // printf("Fragment Number: %d\n", p->frag_no);
+    // printf("Fragment Size: %d\n", p->size);
+    // printf("File Name: %s\n", p->filename);
+    // printf("File Data: %s\n", p->filedata);
+// }
 
-//Prints all of the packets
-void printAllPackets(struct packet * p);
+// //Prints all of the packets
+// void printAllPackets(struct packet * p){
+	// int i,total;
+	// total = p-> 
+    // while(p != NULL){
+        // printPacket(p);
+        // printf("\n");
+        // p = p->next;
+    // }
+// }
 
-//Creates the linked list of packets based on the file
-struct packet * fileConvert(char * filename);
+//packet to string
+char * compressPacket(struct packet * pack, int * length){
+	
+    //Finding size of string
+    int string1 = snprintf(NULL, 0, "%d", pack->total_frag);
+    int string2 = snprintf(NULL, 0, "%d", pack->frag_no);
+    int string3 = snprintf(NULL, 0, "%d", pack->size);
+    int string4 = strlen(pack->filename);
+    int string5 = pack->size;
+    int total_size = string1 + string2 + string3 + string4 + string5;
+    
+    
+    char * compressedPacket = malloc((total_size+4)*sizeof(char));//create space for the string
+    
 
-//Frees the memory that was used for the linked list
-void freePackets(struct packet * root);
+    int index_offset = sprintf(compressedPacket, "%d:%d:%d:%s:",
+                                pack->total_frag, pack->frag_no, pack->size, pack->filename);//store in string
+    
+     
+    memcpy(&compressedPacket[index_offset], pack->filedata, pack->size);//Add the file data
+    
+    
+    *length = index_offset + pack->size;//total length of the packet
+    
+    return compressedPacket;
+}
 
-//Converting the packet to a string format for sending
-char * compressPacket(struct packet * pack, int * len);
+
+//Create linked list of packets
+struct packet * fileConvert(char * file_name){
+    //Initializing vriables
+    FILE * fp;
+    struct packet * head, * prev;
+    int num_bytes, total_fragments, frag_size, frag_num;
+    char data[FRAGMENTSIZE];
+    
+    fp = fopen(file_name, "rb");//Open file in read-mode binary form
+    
+
+    fseek(fp, 0, SEEK_END);//sets cursor to end 
+    num_bytes = ftell(fp);//gets the size of the file
+    fseek(fp, 0, SEEK_SET);//sets the cursor to beginning
+    
+    //Determining the total number of fragments
+    total_fragments = (num_bytes/FRAGMENTSIZE) + 1;
+    
+    //Create linked list of packets 
+    for (frag_num=1; frag_num <= total_fragments; frag_num++) {
+       
+        struct packet * new_node = malloc(sizeof(struct packet));
+        
+		
+        if(frag_num==1){//set root
+            head = new_node;
+        }
+        else{//connect nodes
+            prev->next = new_node;
+        }
+        
+        frag_size = fread(data, 1, FRAGMENTSIZE, fp);//reads file and gets size
+        
+        //set values
+        new_node->total_frag = total_fragments;
+        new_node->frag_no = frag_num;
+        new_node->size = frag_size;
+        new_node->filename = file_name;
+        
+        memcpy(new_node->filedata, data, frag_size);//copying data
+
+        new_node->next = NULL;//add null
+        
+        prev = new_node;//changing prev
+    }
+    
+    fclose(fp);//close file
+    
+    return head;
+}
+
 
 //Converting the packet to its individual elements from string format
-struct packet * extractPacket(char * packet_str);
+struct packet * extractPacket(char * packet_string){
+
+    struct packet * separated_packet;
+    char *total_frag_string, *frag_no_string, *size_string, *file_name, *data;
+    int total_fragments, frag_num, frag_size;
+    
+    //splitting the string
+    total_frag_string = strtok(packet_string, ":");
+    frag_no_string = strtok(NULL, ":");
+    size_string = strtok(NULL, ":");
+    file_name = strtok(NULL, ":");
+    
+    //Converting the strings to int
+    total_fragments = atoi(total_frag_string);
+    frag_num = atoi(frag_no_string);
+    frag_size = atoi(size_string);
+    
+    //index for data
+    int index = strlen(total_frag_string) + strlen(frag_no_string) +
+    strlen(size_string) + strlen(file_name) + 4;
+    
+    //copy data
+    data = malloc(frag_size*sizeof(char));
+    memcpy(data, &packet_string[index], frag_size);
+    
+    //asign values
+    separated_packet = malloc(sizeof(struct packet));
+    separated_packet->total_frag = total_fragments;
+    separated_packet->frag_no = frag_num;
+    separated_packet->size = frag_size;
+    separated_packet->filename = file_name;
+    memcpy(separated_packet->filedata, data, frag_size);
+    
+    
+    if(frag_size < FRAGMENTSIZE){
+        separated_packet->filedata[frag_size] = '\0';//adding null character
+    }
+    return separated_packet;
+}
+
+
+//delete linked list
+void freePackets(struct packet * head){
+    
+    struct packet * curr = head;
+    struct packet * next;
+    
+    //looop till end
+    while(curr != NULL) {
+       
+        next = curr->next;
+        
+        free(curr);//delete
+        
+        curr = next;//set next
+    }
+}
+
 
 #endif
--- lab2/server.c
+++ lab2/server.c
@@ -1,127 +1,110 @@
-//
-//  server.c
-//  FileTransferLab2
-//
-//  Created by Sagar Chadha & Pratiksha Shenoy on 2019-09-24.
-//  Copyright © 2019 Sagar Chadha & Pratiksha Shenoy. All rights reserved.
-//
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <unistd.h>
-
 #include "packet.h"
 
-#define MAXLEN 1100 //Possibly change to 1100
+#define MAXBUFLEN 1100
 
 int main(int argc, char const *argv[]){
-    //Arguments include server and the port number
     
-    //Obtaining port from argument
     int port;
-    switch(argc) {
-        case 2:
-            port = atoi(argv[1]);
-            break;
-        default:
-            fprintf(stderr, "usage: server (UDP listen port)\n");
-            exit(0);
-    }
+	struct sockaddr_in server_addr;
+    char buf[MAXBUFLEN] = {0};
+	int sockfd;//socket file descriptor
+    struct sockaddr_in client_addr;
+	
+	const char msg_yes[]="yes";
+	const char msg_no[]="no";
+	const char msg_ACK[]="ACK";
+	
+	if(argc == 2) //check for correct arguments
+		port = atoi(argv[1]);
+	else{//print error message and return
+		fprintf(stderr, "incorrect argument amount. usage: server <udp listen port>\n");
+		return 1;
+	}
+	 
     
-    int socketDescriptor;
-    
-    //Creates a socket that delivers data
-    if ((socketDescriptor = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+    //Creates a ipv4 dgram udp socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
         fprintf(stderr, "socket error\n");
-        exit(1);
+        return 1;
+    } 
+    
+    
+    server_addr.sin_family = AF_INET;//sets to IPV4
+    
+    server_addr.sin_port = htons(port);//set port to network byte order
+    
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);// references  IP address (in Network Byte Order)
+    
+    memset(server_addr.sin_zero, 0, sizeof(server_addr.sin_zero));//makes sure the struct is empty
+    
+    
+    if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1 ) {//Assigns address to the socket
+        fprintf(stderr, "bind error\n");
+        return 1;
     }
     
-    struct sockaddr_in serverAddress;
     
-    //Signals that we are using IPV4
-    serverAddress.sin_family = AF_INET;
-    //Converts port to a correct format
-    serverAddress.sin_port = htons(port);
-    //**idk
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    //Sets it to the right memory space
-    memset(serverAddress.sin_zero, 0, sizeof(serverAddress.sin_zero));
+    unsigned msg_len = sizeof(struct sockaddr_in);//will contain the length of the address received
     
-    //Assigns address to the socket
-    if (bind(socketDescriptor, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1 ) {
-        fprintf(stderr, "Can't bind name to socket\n");
-        exit(1);
+    if(recvfrom(sockfd, buf, MAXBUFLEN, 0, (struct sockaddr*) &client_addr, &msg_len) == -1) {//receive message from client
+        fprintf(stderr, "recv error\n");
+        return 1;
     }
     
-    char message_buffer[MAXLEN] = {0};
-    struct sockaddr_in clientAddress;
-    unsigned clientMessageLength = sizeof(struct sockaddr_in);
-    
-    //Receives message from client
-    fprintf(stdout, "Server is waiting for message.\n");
-    if(recvfrom(socketDescriptor, message_buffer, MAXLEN, 0, (struct sockaddr*) &clientAddress, &clientMessageLength) < 0) {
-        fprintf(stderr, "Can't receive data\n");
-        exit(1);
-    }
-    
-    //Compares message from client to then send message
-    if (strcmp(message_buffer, "ftp") == 0) {
-        if (sendto(socketDescriptor, "yes", strlen("yes"), 0, (struct sockaddr*) &clientAddress, clientMessageLength) == -1) {
-            fprintf(stderr, "Message not sent to client\n");
-            exit(1);
+    if (strcmp(buf, "ftp") == 0) {//compare the messages received
+        if (sendto(sockfd, msg_yes, strlen("msg_yes"), 0, (struct sockaddr*) &client_addr, msg_len) == -1) {//check if message sent
+                fprintf(stderr, "Message not sent\n");
+                return 1;
         }
-        else if (sendto(socketDescriptor, "no", strlen("no"), 0, (struct sockaddr*) &clientAddress, clientMessageLength) == -1) {
-            fprintf(stderr, "Message not sent to client\n");
-            exit(1);
+        else if (sendto(sockfd, msg_no, strlen(msg_no), 0, (struct sockaddr*) &client_addr, msg_len) == -1) {//check if message sent
+            fprintf(stderr, "Message not sent\n");
+            return 1;
         }
     }
-    
-    //***************Part 2,3 of the Lab*******************
-    FILE* filePointer;
-    int flag = 1;
-    while(flag) {
-        //Receiving message from client
-        int bytesReceived = recvfrom(socketDescriptor, message_buffer, MAXLEN, 0, (struct sockaddr*) &clientAddress, &clientMessageLength);
-        message_buffer[bytesReceived] = '\0';
+
+    FILE* fp;
+    int loop = 1;
+    while(loop) {
+		
+        //receive packets in string format
+        int bytes_recv = recvfrom(sockfd, buf, MAXBUFLEN, 0, (struct sockaddr*) &client_addr, &msg_len);
+        buf[bytes_recv] = '\0';//add null character
         
-        //Creating the packet and extracting the message into packets
-        struct packet* currentPacket = extractPacket(message_buffer);
-        int totalFragments = currentPacket->totalFragments;
-        int fragmentNumber = currentPacket->fragmentNumber;
-        int fragmentSize = currentPacket->fragmentSize;
-        char* fileName = currentPacket->fileName;
-        char* fileData = currentPacket->fileData;
-        printf("Received Packet %d\n", fragmentNumber);
-        //        printf("Total: %d\n", totalFragments);
-        //        printf("File: %s\n", fileName);
-        //        printf("Data: %s\n", fileData);
+        //extracting string and store into struct
+        struct packet* currentPacket = extractPacket(buf);
+        int total_frag = currentPacket->total_frag;
+        int frag_no = currentPacket->frag_no;
+        int size = currentPacket->size;
+        char* filename = currentPacket->filename;
+        char* filedata = currentPacket->filedata;
+        //		printf("Received Packet %d\n", frag_no);
+        //        printf("Total: %d\n", total_frag);
+        //        printf("File: %s\n", filename);
+        //        printf("Data: %s\n", filedata);
         
-        if (fragmentNumber == totalFragments) flag = 0;
-        //First fragment should open the file and begin to write data in binary
-        if(fragmentNumber == 1) {
-            filePointer = fopen(fileName, "wb");
-            printf("Opening File\n");
+		
+        if (frag_no == total_frag) loop = 0;//check loop condition
+		
+
+        if(frag_no == 1) {
+            fp = fopen(filename, "wb");//open/create file in write binary
+            //printf("Opening File\n");
         }
-        fwrite(fileData, 1, fragmentSize, filePointer);
+        fwrite(filedata, 1, size, fp);//write to file
         
-        //Sending an acknowledge response
-        if (sendto(socketDescriptor, "ACK", strlen("ACK"), 0, (struct sockaddr*) &clientAddress, clientMessageLength) == -1) {
+        //Send ACK
+        if (sendto(sockfd, msg_ACK, strlen(msg_ACK), 0, (struct sockaddr*) &client_addr, msg_len) == -1) {
             fprintf(stderr, "Acknowledgement message not sent to client\n");
             exit(1);
         }
         
         //Freeing the current packet pointer
         free(currentPacket);
-        printf("Freed Packet %d\n", fragmentNumber);
-        //Reached the end of the fragments
+        //printf("Freed Packet %d\n", frag_no);
+        
     }
-    fclose(filePointer);
-    
-    //Closes socket
-    close(socketDescriptor);
+    fclose(fp);//close file
+
+    close(sockfd);//close connection
     return 0;
 }
