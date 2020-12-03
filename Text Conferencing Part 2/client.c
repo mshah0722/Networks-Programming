e4778393b075db5f3c228958ce45a7b789b2422b
mshah0722 <malharshah2000@gmail.com>
2020-12-02 22:11:09 -0500

    Small changes

--- Text Conferencing Part 2/client.c
+++ Text Conferencing Part 2/client.c
@@ -28,8 +28,6 @@ BEGIN:
             FD_SET(sockfd, &read_fds);
         }
 
-
-
         if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {            
         }
 
@@ -98,13 +96,12 @@ BEGIN:
                     }
                 }
 				
-				
 				//port string to int
                 port = atoi(port_str);
 
-
 				//Creates a ipv4  socket
                 sockfd = socket(AF_INET, SOCK_STREAM, 0);
+                
                 if (sockfd < 0) fprintf(stderr, "socket error\n");
 
                 struct sockaddr_in *server_addr;
@@ -117,6 +114,7 @@ BEGIN:
 				
 				//connect
                 int conn_fd = connect(sockfd, (const struct sockaddr *)server_addr, sizeof(struct sockaddr_in));
+                
                 if (conn_fd != 0) {
                     printf("Connection error\n");
                     exit(0);
@@ -159,27 +157,27 @@ BEGIN:
                 clientMsg.size = strlen(clientMsg.data);
 				
 				goto SEND_MSG;
-				
             }
             
             else if (strcmp(inputPtr, "/joinsession") == 0){// joinsession command
                 if(session_joined == false){                
-                if (user_logged_in < 0) {
-                    printf("User not logged in\n");
-                    goto BEGIN;
-                }
+                    if (user_logged_in < 0) {
+                        printf("User not logged in\n");
+                        goto BEGIN;
+                    }
 
-                clientMsg.type = JOIN;//set msg type
-                sprintf(clientMsg.source, "%d", user_logged_in);//set user id
+                    clientMsg.type = JOIN;//set msg type
+                    sprintf(clientMsg.source, "%d", user_logged_in);//set user id
 
-                inputPtr = strtok(NULL, " \n");// read input
-                strcpy(clientMsg.data, inputPtr);// store session name
-                clientMsg.size = strlen(clientMsg.data);
-				
-				goto SEND_MSG;
+                    inputPtr = strtok(NULL, " \n");// read input
+                    strcpy(clientMsg.data, inputPtr);// store session name
+                    clientMsg.size = strlen(clientMsg.data);
+                    
+                    goto SEND_MSG;
 			   }	
-            else printf("You're already in a session");
-            goto BEGIN;
+
+                else printf("You're already in a session");
+                goto BEGIN;
             }
             
             else if (strcmp(inputPtr, "/leavesession") == 0){//leavession command
@@ -283,22 +281,22 @@ LOG_OUT:
             }
 SEND_MSG:	
                 if(clientMsg.type != LOGIN){
-				//store values in message struct in appropriate data type
-                char typeStr[5];
-                char sizeStr[5];
-                sprintf(typeStr, "%d", clientMsg.type);
-                sprintf(sizeStr, "%d", clientMsg.size);
+                    //store values in message struct in appropriate data type
+                    char typeStr[5];
+                    char sizeStr[5];
+                    sprintf(typeStr, "%d", clientMsg.type);
+                    sprintf(sizeStr, "%d", clientMsg.size);
 
-				//compress string to send to client
-                char *user_message = struct_to_string(typeStr, sizeStr, clientMsg.source, clientMsg.data);
-				
-				//send to server
-                write(sockfd, user_message, 1000);
-				
-				//free memory
-                free(user_message);
-				
-			if(clientMsg.type == LEAVE_SESS )goto BEGIN;
+                    //compress string to send to client
+                    char *user_message = struct_to_string(typeStr, sizeStr, clientMsg.source, clientMsg.data);
+                    
+                    //send to server
+                    write(sockfd, user_message, 1000);
+                    
+                    //free memory
+                    free(user_message);
+                    
+                if(clientMsg.type == LEAVE_SESS) goto BEGIN;
 				
 			}
         } 
